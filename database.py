69ac03d00302f9ba09226d83493a509d1316328f
DURGA_SURESH_SANABOINA <durgasureshsanaboina@gmail.com>
2026-08-09 15:20:44 +0530

    Fix Render database connection

--- database.py
+++ database.py
@@ -4,15 +4,19 @@ import os
 
 def get_connection():
 
-    connection = psycopg2.connect(
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    # Local development
+    return psycopg2.connect(
         host="localhost",
         database="smart_expense_db",
         user="postgres",
         password=os.getenv("DB_PASSWORD"),
         port="5432"
     )
-
-    return connection
 
 
 if __name__ == "__main__":
